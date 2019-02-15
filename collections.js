25c0d347097c73d28a095d0cd66b1bda9d4b2bf7
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-15 14:21:55 +0200

    mangoose added. MongoDB Athlas added

---
+++ collections.js
@@ -0,0 +1,4 @@
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://Andrey:Andrey@cluster0-shard-00-00-ybpi4.mongodb.net:27017,cluster0-shard-00-01-ybpi4.mongodb.net:27017,cluster0-shard-00-02-ybpi4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const collection = require('./collections')
+
+
 
 app.get('/hello', (req, res, next) => {
-  res.send(req.body)
+  res.send({ 'Hello': 'true' }, collection.get())
 })
 const port = process.env.port || 8080
 app.listen(port, () => console.log(`Server running at ${port} `))

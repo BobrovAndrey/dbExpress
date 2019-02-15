000283225aa5f1ca16eb71c053918c98a52bcbb4
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-15 12:50:29 +0200

    test route hello

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import express from 'express'
+const express = require('express')
 const app = express()
 
-
-
-
-
+app.get('/hello', (req, res, next) => {
+  res.send(req.body)
+})
 const port = process.env.port || 8080
 app.listen(port, () => console.log(`Server running at ${port} `))

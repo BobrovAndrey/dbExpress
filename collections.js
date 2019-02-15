2af241bf94f63b8f422db236a606cc7ad9bae9cd
Andrey Bobrov <Bobrov.law@gmail.com>
2019-02-15 15:07:10 +0200

    mongo DB post

--- collections.js
+++ collections.js
@@ -1,4 +1,13 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://Andrey:Andrey@cluster0-shard-00-00-ybpi4.mongodb.net:27017,cluster0-shard-00-01-ybpi4.mongodb.net:27017,cluster0-shard-00-02-ybpi4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
+const productSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  prcie: Number
+})
+
+mongoose.connect('mongodb://Andrey:Andrey@cluster0-shard-00-00-ybpi4.mongodb.net:27017,cluster0-shard-00-01-ybpi4.mongodb.net:27017,cluster0-shard-00-02-ybpi4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  { useNewUrlParser: true })
+
+module.exports = mongoose.model('Product', productSchema)
--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const express = require('express')
 const app = express()
 const collection = require('./collections')
-
-
+const Product = require('./collections')
+const mongoose = require('mongoose')
 
 app.get('/hello', (req, res, next) => {
   res.send({ 'Hello': 'true' }, collection.get())
 })
+
+app.post('/', (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: 'req.body.name',
+    price: '287'
+  })
+  product
+    .save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// app.post('/', (req, res, next) => {
+//   const product = new Product({
+//     _id: new mongoose.Types.ObjectId(),
+//     price: req.body.price
+
+//   })
+//   product.save().than(result => { console.log(result) })
+//     .catch(err => console.log(err))
+//   res.status(201).json('Handling post request')
+// })
 const port = process.env.port || 8080
 app.listen(port, () => console.log(`Server running at ${port} `))

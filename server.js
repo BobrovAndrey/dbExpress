2114b97ff1a946e9a9c939ecd8d35d0d3615ef77
Andrey <Bobrov.law@gmail.com>
2019-02-17 15:07:05 +0200

    Revert "service implementations added"
    This reverts commit 61717525f44053d82334cbf8d2f5289aea9c48a5.

--- server.js
+++ server.js
@@ -1,57 +1,39 @@
-
 const express = require('express')
 const app = express()
-// const mongoose = require('mongoose')
-const ProductModel = require('./collections').ProductModel
-const bodyParser = require('body-parser')
+const collection = require('./collections')
+const Product = require('./collections')
+const mongoose = require('mongoose')
 
-console.log(ProductModel)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-app.get('/', (req, res, next) => {
-  res.send({ 'testPassed': 'true' })
-  console.log(req.body)
+app.get('/hello', (req, res, next) => {
+  res.send({ 'Hello': 'true' }, collection.get())
 })
 
 app.post('/', (req, res, next) => {
-  console.log('Brand is ', req.body.brand)
-  console.log('units is ', req.body.units)
-  console.log('expDate is ', req.body.expDate)
-  if (req.body.brand && req.body.units && req.body.expDate) {
-    const product = new ProductModel({
-    // _id: new mongoose.Types.ObjectId(),
-      brand: req.body.brand,
-      units: req.body.units,
-      expDate: req.body.expDate
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: 'req.body.name',
+    price: '287'
+  })
+  product
+    .save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: result
+      });
     })
-    product
-      .save()
-      .then(result => {
-        console.log(result)
-        res.status(201).json({
-          message: 'Handling POST requests to /products',
-          createdProduct: result
-        })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
       })
-      .catch(err => {
-        console.log(err)
-        res.status(500).json({
-          error: err
-        })
-      })
-  } else {
-    res.status(400).json({
-      message: 'Some fields are missing'
     })
-  }
 })
 
 app.get('/:productId', (req, res, next) => {
   const id = req.params.productId
-  ProductModel.findById(id) //.populate('product')
+  Product.findById(id)
     .exec()
     .then(doc => {
       console.log(doc)

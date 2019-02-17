3f766da6df18474d3f37182e3c452212e12d1ae8
Andrey <Bobrov.law@gmail.com>
2019-02-17 17:39:30 +0200

    get, post, delete && populate added

--- collections.js
+++ collections.js
@@ -1,13 +1,35 @@
-
 const mongoose = require('mongoose')
 
-const productSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  prcie: Number
+// Athlas usage
+// mongoose.connect('mongodb://Andrey:Andrey@cluster0-shard-00-00-ybpi4.mongodb.net:27017,cluster0-shard-00-01-ybpi4.mongodb.net:27017,cluster0-shard-00-02-ybpi4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+//  { useNewUrlParser: true })
+
+// Local usage
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+const db = mongoose.connection
+const Schema = mongoose.Schema
+db.on('error', (error) => {
+  console.log('Error was occured', error.message)
 })
 
-mongoose.connect('mongodb://Andrey:Andrey@cluster0-shard-00-00-ybpi4.mongodb.net:27017,cluster0-shard-00-01-ybpi4.mongodb.net:27017,cluster0-shard-00-02-ybpi4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
-  { useNewUrlParser: true })
+db.once('open', () => {
+  console.log('Connected to DB')
+})
 
-module.exports = mongoose.model('Product', productSchema)
+const userSchema = Schema({
+  name: String,
+  lastname: String,
+  salary: Number,
+  skills: [String],
+  product: { type: String, ref: 'Product' }
+})
+const productSchema = Schema({
+  brand: String,
+  units: String,
+  expDate: Date
+})
+
+const UserModel = mongoose.model('User', userSchema)
+const ProductModel = mongoose.model('Product', productSchema)
+
+module.exports = { UserModel, ProductModel }
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
+
 const express = require('express')
 const app = express()
-const collection = require('./collections')
-const Product = require('./collections')
-const mongoose = require('mongoose')
+const ProductModel = require('./collections').ProductModel
+const UserModel = require('./collections').UserModel
 
-app.get('/hello', (req, res, next) => {
-  res.send({ 'Hello': 'true' }, collection.get())
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// Get
+app.get('/', (req, res, next) => {
+  res.send({ 'testPassed': 'true' })
+  console.log(req.body)
 })
 
-app.post('/', (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: 'req.body.name',
-    price: '287'
-  })
-  product
-    .save()
-    .then(result => {
-      console.log(result)
-      res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: result
-      });
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({
-        error: err
-      })
-    })
-})
-
-app.get('/:productId', (req, res, next) => {
+app.get('/user/:productId', (req, res, next) => {
   const id = req.params.productId
-  Product.findById(id)
+  UserModel.findById(id)
     .exec()
     .then(doc => {
       console.log(doc)
@@ -45,15 +31,94 @@ app.get('/:productId', (req, res, next) => {
     })
 })
 
-// app.post('/', (req, res, next) => {
-//   const product = new Product({
-//     _id: new mongoose.Types.ObjectId(),
-//     price: req.body.price
+app.get('/user/', (req, res, next) => {
+  UserModel.find()
+    .populate('product')
+    .exec()
+    .then(doc => {
+      console.log(doc)
+      res.status(200).json(doc)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
+})
 
-//   })
-//   product.save().than(result => { console.log(result) })
-//     .catch(err => console.log(err))
-//   res.status(201).json('Handling post request')
-// })
+// Post
+app.post('/product', (req, res, next) => {
+  if (req.body.brand && req.body.units && req.body.expDate) {
+    const product = new ProductModel({
+      brand: req.body.brand,
+      units: req.body.units,
+      expDate: req.body.expDate
+    })
+    product
+      .save()
+      .then(result => {
+        console.log(result)
+        res.status(201).json({
+          message: 'Handling POST requests to /products',
+          createdProduct: result
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({
+          error: err
+        })
+      })
+  } else {
+    res.status(400).json({
+      message: 'Some fields are missing'
+    })
+  }
+})
+
+app.post('/user', (req, res, next) => {
+  if (req.body.name && req.body.lastname && req.body.salary && req.body.skills && req.body.product) {
+    const user = new UserModel({
+      name: req.body.name,
+      lastname: req.body.lastname,
+      salary: req.body.salary,
+      skills: req.body.skills,
+      product: req.body.product
+    })
+    user
+      .save()
+      .then(result => {
+        console.log(result)
+        res.status(201).json({
+          message: 'Handling POST requests to /products',
+          createdProduct: result
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({
+          error: err
+        })
+      })
+  } else {
+    res.status(400).json({
+      message: 'Some fields are missing'
+    })
+  }
+})
+
+// Delete
+app.delete('/user/', (req, res, next) => {
+  UserModel.remove()
+    .exec()
+    .then(doc => {
+      console.log(doc)
+      res.status(200).json(doc)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
+})
+
 const port = process.env.port || 8080
 app.listen(port, () => console.log(`Server running at ${port} `))

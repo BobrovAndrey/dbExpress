e8beaf31dba6aa1f511da22760d6aab688a38e5b
Andrey <Bobrov.law@gmail.com>
2019-02-18 11:48:55 +0200

    pretify server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/', (req, res, next) => {
   console.log(req.body)
 })
 
+// Find one by ID
 app.get('/user/:productId', (req, res, next) => {
   const id = req.params.productId
   UserModel.findById(id)
@@ -44,7 +45,20 @@ app.get('/users/', (req, res, next) => {
     })
 })
 
-// Populate
+// Find all products
+app.get('/product', (req, res, next) => {
+  ProductModel.find()
+    .then(doc => {
+      console.log(doc)
+      res.status(200).json(doc)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
+})
+
+// Populate (user with product)
 app.get('/user', (req, res, next) => {
   UserModel.find()
     .populate('product')
@@ -59,7 +73,7 @@ app.get('/user', (req, res, next) => {
     })
 })
 
-// Post
+// Post product
 app.post('/product', (req, res, next) => {
   if (req.body.brand && req.body.units && req.body.expDate) {
     const product = new ProductModel({
@@ -89,6 +103,7 @@ app.post('/product', (req, res, next) => {
   }
 })
 
+// Post user
 app.post('/user', (req, res, next) => {
   if (req.body.name && req.body.lastname && req.body.salary && req.body.skills && req.body.product) {
     const user = new UserModel({
@@ -134,18 +149,7 @@ app.delete('/user/', (req, res, next) => {
     })
 })
 
-// Find one specified user
-app.get('/user/find', (req, res, next) => {
-  const requestName = req.body.name
-  const requestLastname = req.body.lastname
-  UserModel.findOne({ name: `${requestName}`, lastname: `${requestLastname}` })
-    .then(res.status(200).json({
-      userFound: 'true'
-    }))
-    .catch(err => console.log(err))
-})
-
-// Put (replace)
+// Put (replace user)
 app.put('/user/:id', (req, res, next) => {
   const userId = req.params.id
   const user = {
@@ -157,9 +161,9 @@ app.put('/user/:id', (req, res, next) => {
   }
   UserModel.findOne({ _id: userId })
     .then(doc => {
-      console.log('found user id: ', userId)
-      console.log('here is obj: ', doc)
-      console.log('user object is: ', user)
+      console.log(' ---> found user id: ', userId)
+      console.log(' ---> here is obj: ', doc)
+      console.log(' ---> user object is: ', user)
       UserModel.replaceOne({ _id: userId }, user)
         .then(doc => {
           res.status(200).json(doc)
@@ -169,7 +173,27 @@ app.put('/user/:id', (req, res, next) => {
     .catch(err => console.log(err))
 })
 
-// Patch (modify)
+// Put (replace product)
+app.put('/product/:id', (req, res, next) => {
+  const productId = req.params.id
+  const product = {
+    brand: req.body.brand,
+    units: req.body.units,
+    expDate: req.body.expDate
+  }
+  ProductModel.findOne({ _id: productId })
+    .then(doc => {
+      console.log(' ---> found product id: ', productId)
+      console.log(' ---> here is obj: ', doc)
+      console.log(' ---> user object is: ', product)
+      ProductModel.replaceOne({ _id: productId }, product)
+        .then(doc => {
+          res.status(200).json(doc)
+        })
+    }
+    )
+    .catch(err => console.log(err))
+})
 
 const port = process.env.port || 8080
 app.listen(port, () => console.log(`Server running at ${port} `))
